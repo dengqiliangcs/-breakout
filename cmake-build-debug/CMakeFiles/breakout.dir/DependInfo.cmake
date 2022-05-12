5c731443c19116e0e6631a06af13d82705801866
dengqiliangcs <675213153@qq.com>
2022-05-12 17:15:48 +0800

    new added files, add detail info.

---
+++ cmake-build-debug/CMakeFiles/breakout.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/ball_object_collisions.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/ball_object_collisions.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/game.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/game.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/game_level.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/game_level.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/game_object.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/game_object.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/main.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/main.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/particle_generator.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/particle_generator.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/post_processor.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/post_processor.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/power_up.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/power_up.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/resource_manager.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/resource_manager.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/shader.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/shader.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/sprite_renderer.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/sprite_renderer.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/text_renderer.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/text_renderer.cpp.o"
+  "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/texture.cpp" "/Users/dql/学习资料/C++代码/Clion/opengl/breakout/cmake-build-debug/CMakeFiles/breakout.dir/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/glew/2.2.0_1/include/GL"
+  "/usr/local/Cellar/glfw/3.3.2/include/GLFW"
+  "/opt/local/include/SOIL"
+  "/usr/local/Cellar/glm"
+  "/usr/local/include"
+  "/usr/local/Cellar/freetype/2.10.4/include/freetype2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/breakout.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/breakout.dir/ball_object_collisions.cpp.o"
+  "CMakeFiles/breakout.dir/game.cpp.o"
+  "CMakeFiles/breakout.dir/game_level.cpp.o"
+  "CMakeFiles/breakout.dir/game_object.cpp.o"
+  "CMakeFiles/breakout.dir/main.cpp.o"
+  "CMakeFiles/breakout.dir/particle_generator.cpp.o"
+  "CMakeFiles/breakout.dir/post_processor.cpp.o"
+  "CMakeFiles/breakout.dir/power_up.cpp.o"
+  "CMakeFiles/breakout.dir/resource_manager.cpp.o"
+  "CMakeFiles/breakout.dir/shader.cpp.o"
+  "CMakeFiles/breakout.dir/sprite_renderer.cpp.o"
+  "CMakeFiles/breakout.dir/text_renderer.cpp.o"
+  "CMakeFiles/breakout.dir/texture.cpp.o"
+  "breakout"
+  "breakout.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/breakout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
